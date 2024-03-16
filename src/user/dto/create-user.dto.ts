c0a82ca50bfc5c57106f1a8c8085f72f25bd78bb
Alexandre Costa <alexandreodpc@gmail.com>
2024-03-16 13:31:15 -0300

    feat(user): cadastro

---
+++ src/user/dto/create-user.dto.ts
@@ -0,0 +1,24 @@
+import { User } from '../entities/user.entity';
+import {
+  IsEmail,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
+export class CreateUserDto extends User {
+  @IsString()
+  name: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
+  @Matches(/(?=.*[a-z]).*$/, {
+    message: 'weak password',
+  })
+  password: string;
+}
---
+++ src/user/user.service.ts
@@ -0,0 +1,29 @@
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { Prisma } from '@prisma/client';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class UserService {
+  constructor(private prisma: PrismaService) {}
+
+  async create(createUserDto: CreateUserDto) {
+    const user = await this.findByEmail(createUserDto.email);
+    if (user) throw new BadRequestException('user already created');
+
+    const data: CreateUserDto = {
+      ...createUserDto,
+      password: await bcrypt.hash(createUserDto.password, 16),
+    };
+
+    await this.prisma.user.create({ data });
+
+    return 'user created';
+  }
+
+  findByEmail(email: string) {
+    return this.prisma.user.findUnique({ where: { email } });
+  }
+}
